afb8bb4cf976bb70f0abff62712e526a29a024c1
Dalton Duncan <daltonduncan44@gmail.com>
2018-07-24 21:14:45 -0400

    add seudo

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+//have timer start when button is clicked
+//take in user inputs
+//check if inputs are correct
+//end test when submit is clicked
+//use inputs to display how many were right and wrong. 
