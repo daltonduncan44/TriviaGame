dff692242346405935bfa01c81b9ae87f997b57c
Dalton Duncan <daltonduncan44@gmail.com>
2018-07-26 12:04:46 -0400

    start button working alert removed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,37 @@
-$( document ).ready(function() {
-  console.log( "ready!" );
+$(document).ready(function () {
+  console.log("ready!");
+var counter = 30;
+$("#count").text(counter);
 
 
 
 
-//have timer start when button is clicked
-$(".start btn btn-secondary btn-lg btn-block").on("click", function(){
-    var counter = 30;
-    setInterval(function() {
+  //have timer start when button is clicked
+ 
+  $("#startClock").click(function () {
+    //var counter = 30;
+    setInterval(function () {
       counter--;
-       if (counter >= 0) {
-          span = document.getElementById("count");
-          span.innerHTML = counter;
+      if (counter >= 0) {
+        span = document.getElementById("count");
+        span.innerHTML = counter;
         console.log("say something")
-       }
-       if (counter === 0) {
-          alert('sorry, out of time');
-          clearInterval(counter);
-        }
-      }, 1000);
- });
-
-
+      }
+      if (counter === 0) {
+        alert('sorry, out of time');
+        clearInterval(counter);
+      }
+    }, 1000);
+  });
     
-//take in user inputs
-//check if inputs are correct
+ 
 
-//var userAnswer = $("#quetion1").val();
-//if(userAnswer){}
-//end test when submit is clicked
-//use inputs to display how many were right and wrong. 
+
+  //take in user inputs
+  //check if inputs are correct
+
+  //var userAnswer = $("#quetion1").val();
+  //if(userAnswer){}
+  //end test when submit is clicked
+  //use inputs to display how many were right and wrong. 
 });
