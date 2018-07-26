304194c3d72ad1ff4d5f74463b41549ee23f8f56
Dalton Duncan <daltonduncan44@gmail.com>
2018-07-26 12:26:16 -0400

    working on grading input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   console.log("ready!");
+//===global vars
 var counter = 30;
+var rightAns = 0; 
+// display the counter to the doc
 $("#count").text(counter);
 
 
@@ -9,13 +12,15 @@ $("#count").text(counter);
   //have timer start when button is clicked
  
   $("#startClock").click(function () {
+    console.log("on the clock")
     //var counter = 30;
     setInterval(function () {
+      // subtract counter
       counter--;
       if (counter >= 0) {
         span = document.getElementById("count");
         span.innerHTML = counter;
-        console.log("say something")
+
       }
       if (counter === 0) {
         alert('sorry, out of time');
@@ -24,12 +29,24 @@ $("#count").text(counter);
     }, 1000);
   });
     
- 
+  $("#submitBttn").click(function () 
+  {
+console.log("test submitted");
+    clearInterval(counter);
+    inputGrading();
+  });
+
+
 
 
   //take in user inputs
   //check if inputs are correct
-
+  function inputGrading () {
+    if(radio == value("true")) {
+rightAns++;
+console.log(rightAns);
+    }
+}
   //var userAnswer = $("#quetion1").val();
   //if(userAnswer){}
   //end test when submit is clicked
